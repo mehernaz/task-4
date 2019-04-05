dc6989f3032b7ec21d8e937ae9d1b09a20f31a44
mehernaz <38257529+mehernaz@users.noreply.github.com>
2019-04-05 17:00:29 +0530

    Update t1.java

--- t1.java
+++ t1.java
@@ -1,5 +1,27 @@
 package epam4;
 
+import java.util.*;
 public class t1 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+
+String s=sc.nextLine();
+boolean fl=false;
+String h[]= {"H","Q","+","9"};
+for(int i=0;i<4;i++)
+{
+	if(s.contains(h[i]))
+	{
+		fl=true;
+	}
+}
+if(fl==true)
+{
+	System.out.println("YES");
+}
+else
+	System.out.println("NO");
+	}
 }
